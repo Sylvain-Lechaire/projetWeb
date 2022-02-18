8101350146d3f379c1f7def6c0fac89b90a57e0b
EthannSchneider <ethann.schneider@cpnv.ch>
2022-02-18 09:11:50 +0100

    Added Register and Modified Login

--- Source Code/View/Login.php
+++ Source Code/View/Login.php
@@ -2,7 +2,7 @@
 /*
 Projet: Tank&Cio
 Author: Ethann Schneider
-Version: 1.0
+Version: 1.0.1
 date: 04.02.22
 */
 
@@ -28,14 +28,14 @@ if(isset($_SESSION['username']) && isset($_SESSION['password'])){
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('vendor/bg.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
-				<span class="login100-form-title p-b-41">Account Login</span>
+				<span class="login100-form-title p-b-41">Login</span>
 				<form class="login100-form validate-form p-b-33 p-t-5" method="post" action="../Controller/connect.php">
 					<div class="wrap-input100 validate-input" data-validate="Enter username">
-						<input class="input100" type="text" name="username" placeholder="User name">
+						<input class="input100" type="email" name="username" placeholder="Email / Username" required>
 						<span class="focus-input100" data-placeholder=""></span>
 					</div>
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="password" placeholder="Password">
+						<input class="input100" type="password" name="password" placeholder="Password" required>
 						<span class="focus-input100" data-placeholder=""></span>
 					</div>
 					<div class="container-login100-form-btn m-t-32">
--- Source Code/View/Register.php
+++ Source Code/View/Register.php
@@ -2,7 +2,7 @@
 /*
 Projet: Tank&Cio
 Author: Ethann Schneider
-Version: 1.0
+Version: 1.0.1
 date: 04.02.22
 */
 
@@ -16,30 +16,52 @@ if(isset($_SESSION['username']) && isset($_SESSION['password'])){
 <html lang="fr">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<title>Login</title>
+	<title>Register</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="assets/images/header-logo.png" />
 	<link rel="stylesheet" type="text/css" href="vendor/icon-font.css">
 	<link rel="stylesheet" type="text/css" href="vendor/util.css">
 	<link rel="stylesheet" type="text/css" href="vendor/main.css">
+    <script type="text/javascript">
+        function Validate() {
+            var password = document.getElementById("password").value;
+            var confirmPassword = document.getElementById("confirmPassword").value;
+            if (password != confirmPassword) {
+                return false;
+            }
+            return true;
+        }
+    </script>
 </head>
 <body>
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('vendor/bg.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
-				<span class="login100-form-title p-b-41">Account Login</span>
-				<form class="login100-form validate-form p-b-33 p-t-5" method="post" action="../Controller/connect.php">
+				<span class="login100-form-title p-b-41">New Account</span>
+				<form class="login100-form validate-form p-b-33 p-t-5" method="post" action="../Controller/Register.php">
 					<div class="wrap-input100 validate-input" data-validate="Enter username">
-						<input class="input100" type="text" name="username" placeholder="User name">
+						<input class="input100" type="email" name="username" placeholder="Email" required>
 						<span class="focus-input100" data-placeholder=""></span>
 					</div>
+                    <div class="wrap-input100 validate-input" data-validate="Enter firstName">
+                        <input class="input100" type="text" name="realName" placeholder="first name" required>
+                        <span class="focus-input100" data-placeholder=""></span>
+                    </div>
+                    <div class="wrap-input100 validate-input" data-validate="Enter secondName">
+                        <input class="input100" type="text" name="familyName" placeholder="second name" required>
+                        <span class="focus-input100" data-placeholder=""></span>
+                    </div>
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="password" placeholder="Password">
+						<input class="input100" type="password" id="password" name="password" placeholder="Password" required>
 						<span class="focus-input100" data-placeholder=""></span>
 					</div>
+                    <div class="wrap-input100 validate-input" data-validate="confirm password">
+                        <input class="input100" type="password" id="confirmPassword" placeholder="confirm password" required>
+                        <span class="focus-input100" data-placeholder=""></span>
+                    </div>
 					<div class="container-login100-form-btn m-t-32">
-						<button class="login100-form-btn">Login</button>
+						<button onclick="return Validate()" class="login100-form-btn">Register</button>
 					</div>
 				</form>
 			</div>
