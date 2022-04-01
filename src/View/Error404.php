7a589b03040e2dcabd91072f2f22a7ce3d5383a3
EthannSchneider <ethann@shkermit.ch>
2022-04-01 23:58:22 +0200

    Added View/Error404.php

---
+++ src/View/Error404.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Projet: Tank&Cio
+Author: Ethann Schneider
+Version: 1.0
+date: 04.02.22
+*/
+
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+      <title>Contact</title>
+      <?php include 'template/head.php'?>
+  </head>
+
+  <body>
+
+  <?php include "template/header.php";?>
+
+    <!-- Page Content -->
+    <!-- About Page Starts Here -->
+    <div style="text-align: center;" class="error-page">
+        <br><br><br>
+        <h1 style="color: darkred;">Error 404</h1> Not Found
+        <br><br><br><br><br><br><br><br>
+    </div>
+    <!-- About Page Ends Here -->
+
+  <?php include "template/footer.html";?>
+
+
+  </body>
+
+</html>
